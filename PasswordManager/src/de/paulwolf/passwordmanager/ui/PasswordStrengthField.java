605e7b2ab4d752c534ce8d605d80d6552072a543
Paul Wolf <Luqus@gmx.net>
2022-06-19 20:25:59 +0200

    Implemented zxcvbn

--- PasswordManager/src/de/paulwolf/passwordmanager/ui/PasswordStrengthField.java
+++ PasswordManager/src/de/paulwolf/passwordmanager/ui/PasswordStrengthField.java
@@ -1,18 +1,15 @@
 package de.paulwolf.passwordmanager.ui;
 
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.regex.Pattern;
 
 public class PasswordStrengthField extends JPasswordField implements KeyListener {
-
-    private static double log2(double N) {
-
-        return Math.log(N) / Math.log(2);
-    }
 
     public PasswordStrengthField(int c) {
 
@@ -22,45 +19,24 @@ public class PasswordStrengthField extends JPasswordField implements KeyListener
 
     public void evaluatePassword() {
 
-        double passwordEntropy;
-        int characterPool = 0;
-
         String password = new String(this.getPassword());
 
-        Pattern lowercase = Pattern.compile("[a-z]");
-        Pattern uppercase = Pattern.compile("[A-Z]");
-        Pattern digits = Pattern.compile("\\d");
-        Pattern specials = Pattern.compile("[!\"$%&/()=?{\\[\\]}\\\\+*~#\\-_.:,;|<>']");
-
-        if (lowercase.matcher(password).find())
-            characterPool += 26;
-        if (uppercase.matcher(password).find())
-            characterPool += 26;
-        if (digits.matcher(password).find())
-            characterPool += 10;
-        if (specials.matcher(password).find())
-            characterPool += 29;
-
-        passwordEntropy = log2(Math.pow(characterPool, password.length()));
-        passwordEntropy = Math.min(128, passwordEntropy);
+        Zxcvbn zxcvbn = new Zxcvbn();
+        Strength strength = zxcvbn.measure(password);
 
         if (password.length() == 0) {
             this.setBorder(UIManager.getBorder("TextField.border"));
             return;
         }
 
-        this.setBorder(new LineBorder(Color.getHSBColor((float) ((passwordEntropy / 1.28d * 3.6d) / 3 / 360), 1f, 0.7f), 2));
+        this.setBorder(new LineBorder(Color.getHSBColor(1.0f / 3.0f / 4.0f * strength.getScore(), 1f, 0.7f), 2));
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-        evaluatePassword();
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
-    public void keyPressed(KeyEvent e) {
-        evaluatePassword();
-    }
+    public void keyPressed(KeyEvent e) {}
 
     @Override
     public void keyReleased(KeyEvent e) {
