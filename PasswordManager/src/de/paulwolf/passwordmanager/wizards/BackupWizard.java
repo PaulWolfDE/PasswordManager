9d227eee6dd39adb77cfb2af8a48d5a5ad06c70f
Paul Wolf <Luqus@gmx.net>
2022-07-06 19:49:28 +0200

    Added backup concept

---
+++ PasswordManager/src/de/paulwolf/passwordmanager/wizards/BackupWizard.java
@@ -0,0 +1,21 @@
+package de.paulwolf.passwordmanager.wizards;
+
+/**
+ * SFTP AUTOMATED BACKUP CONCEPT
+ * -----------------------------
+ *
+ * special reserved entry for sftp/ssh credentials;
+ * backup to remote server every safe (later maybe user-defined);
+ * inform user about unsuccessful saves
+ *
+ * reserved entry structure:
+ * TITLE: sftp-automated-backup (reserved)
+ * USERNAME: (sftp/ssh user name)
+ * EMAIL -> HOSTNAME: (sftp/ssh server name)
+ * PASSWORD: (sftp/ssh secret)
+ * LAST MODIFIED: [N/A or ignored]
+ * NOTES: [N/A or ignored]
+ */
+
+public class BackupWizard {
+}
