25ba09da302caabdc04734fb471065f90efca4cc
Paul Wolf <Luqus@gmx.net>
2023-07-25 19:00:45 +0200

    Fixed the border for the password strength field with new design

--- PasswordManager/src/de/paulwolf/passwordmanager/ui/passwordfields/PasswordStrengthField.java
+++ PasswordManager/src/de/paulwolf/passwordmanager/ui/passwordfields/PasswordStrengthField.java
@@ -3,12 +3,11 @@ package de.paulwolf.passwordmanager.ui.passwordfields;
 import com.nulabinc.zxcvbn.Strength;
 import com.nulabinc.zxcvbn.Zxcvbn;
 import de.paulwolf.passwordmanager.Configuration;
-import de.paulwolf.passwordmanager.Main;
 import de.paulwolf.passwordmanager.ui.components.ScaledPasswordField;
-import de.paulwolf.passwordmanager.ui.components.ScaledTextField;
 import de.paulwolf.passwordmanager.wizards.EncodingWizard;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.KeyListener;
@@ -47,7 +46,30 @@ abstract public class PasswordStrengthField extends ScaledPasswordField implemen
 
         Arrays.fill(password, (byte) 0);
 
-        this.setBorder(new LineBorder(Color.getHSBColor(1.0f / 3.0f / 4.0f * strength.getScore(), 1f, 0.7f), 2));
+        Border existingBorder = this.getBorder();
+        Border coloredBorder = new LineBorder(Color.getHSBColor(1.0f / 3.0f / 4.0f * strength.getScore(), 1f, 0.7f), 3);
+        Border passwordStrengthBorder = new Border() {
+            @Override
+            public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                // Die vorhandene Border zeichnen und dabei die gewünschte Farbe verwenden
+                existingBorder.paintBorder(c, g, x, y, width, height);
+                coloredBorder.paintBorder(c, g, x, y, width, height);
+            }
+
+            @Override
+            public Insets getBorderInsets(Component c) {
+                // Die vorhandenen Border-Insets zurückgeben
+                return existingBorder.getBorderInsets(c);
+            }
+
+            @Override
+            public boolean isBorderOpaque() {
+                // Die Opaque-Eigenschaft der vorhandenen Border zurückgeben
+                return existingBorder.isBorderOpaque();
+            }
+        };
+
+        this.setBorder(passwordStrengthBorder);
     }
 
     public void setDisplayPasswordStrength(boolean displayPasswordStrength) {
